8c964fda0ccb667e2952f186a6a9771c49b7bfcf
shaikhdanish9702 <shaikhdanish9702@gmail.com>
2022-09-06 15:36:09 +0530

    first commit

--- array2.java
+++ array2.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class array2 {
-    System.out.println("enter a length of array");
-    int length = sc.nextInt();System.out.println("enter a number with giving space");
-    int[] arr = new int[length];
+    public static void main(String[] args) {
+        System.out.println("enter a length of array");
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        System.out.println("enter a number with giving space");
+        int[] arr = new int[length];
 
-    for(
-    int i = 0;i<length;i++)
-    {
-        int enteredNum = sc.nextInt();
-        arr[i] = enteredNum;
+        for (int i = 0; i < length; i++) {
+            int enteredNum = sc.nextInt();
+            arr[i] = enteredNum;
+        }
     }
+
 }
